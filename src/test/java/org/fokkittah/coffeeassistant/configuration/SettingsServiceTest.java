0787b4eaa5e8505601a9405a7f52d9fb1a8528b7
Fokkittah <98163912+Fokkittah@users.noreply.github.com>
2024-06-08 00:17:20 +0200

    test cleanup

--- src/test/java/org/fokkittah/coffeeassistant/configuration/SettingsServiceTest.java
+++ src/test/java/org/fokkittah/coffeeassistant/configuration/SettingsServiceTest.java
@@ -1,17 +1,71 @@
 package org.fokkittah.coffeeassistant.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.fokkittah.coffeeassistant.configuration.recipe.Recipe;
+import org.fokkittah.coffeeassistant.configuration.recipe.Step;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
 public class SettingsServiceTest {
-    SettingsService settingsService = new SettingsService();
+
+    @Mock
+    private ObjectMapper xmlMapper;
+
+    private SettingsService settingsService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        settingsService = new SettingsService();
+        settingsService.xmlMapper = xmlMapper;
+    }
 
     @Test
-    public void testService() throws IOException {
-        Settings settings = settingsService.loadSettings("src/test/resources/settings.xml");
-        settingsService.saveSettings(settings, "src/test/resources/settingsOutput.xml");
-        Settings testLoad = settingsService.loadSettings("src/test/resources/settingsOutput.xml");
-        System.out.println(settings);
+    public void summarizeTotalWaterInRecipe() {
+        Recipe recipe = new Recipe();
+        recipe.setSteps(Arrays.asList(new Step(1, 2, "desc1"), new Step(3, 4, "desc2")));
+        assertEquals(6, settingsService.summarizeTotalWaterInRecipe(recipe));
     }
+
+    @Test
+    public void summarizeTotalTimeInRecipeInSeconds() {
+        Recipe recipe = new Recipe();
+        recipe.setSteps(Arrays.asList(new Step(1, 2, "desc1"), new Step(3, 4, "desc2")));
+        assertEquals(4, settingsService.summarizeTotalTimeInRecipeInSeconds(recipe));
+    }
+
+    @Test
+    public void loadSettings() throws IOException {
+        Settings expectedSettings = new Settings();
+        when(xmlMapper.readValue(new File("src/test/resources/settings.xml"), Settings.class)).thenReturn(expectedSettings);
+        Settings actualSettings = settingsService.loadSettings("src/test/resources/settings.xml");
+        assertEquals(expectedSettings, actualSettings);
+    }
+
+    @Test
+    public void saveSettingsToFile() throws IOException {
+        Settings settings = new Settings();
+        settingsService.saveSettings(settings, "src/test/resources/testSettings.xml");
+        verify(xmlMapper, times(1)).writeValue(any(File.class), eq(settings));
+    }
+
+    @Test
+    public void getSettingsReturnsExistingSettings() {
+        Settings settings = new Settings();
+        settingsService.setSettings(settings);
+        assertEquals(settings, settingsService.getSettings());
+    }
+
 }
